9e22476af4d2ced420e10e27aa00e003418e4bc9
yongjjang <pastelhawk@gmail.com>
2019-10-21 21:21:57 +0900

    NDEBUG Update

--- assert 구현/my_assert.c
+++ assert 구현/my_assert.c
@@ -2,12 +2,14 @@
 #include <assert.h>
 #include <stdlib.h>
 
-#define my_assert(expr) \
-  ((void) ((expr) ? 0 : (__assert_fail (#expr, __FILE__, __LINE__, __func__), 0)))
+
+#ifndef NDEGUG
+  #define my_assert(expr)						\
+    ((void) ((expr) ? 0 : (__assert_fail (#expr, __FILE__, __LINE__, __func__), 0)))
+#endif
 
 void foo(int num){
   my_assert(((num >= 0) && (num <= 100)));
-
   printf("foo : num = %d\n", num);
   
 }
